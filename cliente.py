0c771ac4f89971fee5d2d5f311e31647752bb601
Cainan Lima <80228031+cainanneves@users.noreply.github.com>
2023-11-28 11:38:22 -0300

    adiciona login no sistema

--- cliente.py
+++ cliente.py
@@ -9,38 +9,79 @@ def main():
     client_socket.connect((HOST, PORT))
 
     # Solicita o número de RG ao usuário para autenticação
-    rg = input("Digite seu número de RG: ")
-    client_socket.send(rg.encode())
-
     while True:
-        # Menu de operações bancárias para o cliente
-        print("Menu:")
-        print("1. Consultar saldo")
-        print("2. Realizar retirada")
-        print("3. Realizar transferência")
-        print("4. Sair")
-        choice = input("Escolha a operação: ")
+        # Menu de Login
+        print("Login")
+        print("1. Realizar Login")
+        print("2. Cadastrar conta")
+        print("3. Encerrar Conexão")
+        loginChoice = input("Escolha a operação: ")
 
-        if choice == "4":
+        if loginChoice == "3":
             client_socket.close()
             break
 
-        # Envia a escolha do cliente para o servidor
-        client_socket.send(choice.encode())
+        # Envia a escolha de login ou cadastro
+        client_socket.send(loginChoice.encode())
 
-        if choice == "2":
-            # Solicita o valor a ser retirado
-            amount = input("Digite o valor: ")
-            client_socket.send(amount.encode())
+        if loginChoice == "2":
 
-        elif choice == "3":
-            # Solicita o valor e o número de RG da conta de destino
-            amount = input("Digite o valor: ")
-            dest_rg = input("Digite o número de RG da conta de destino: ")
-            client_socket.send(amount.encode())
-            client_socket.send(dest_rg.encode())
+            rg = input("Digite o seu RG: ")
+            senha = input("Digite sua senha:")
+            client_socket.send(senha.encode())
+            client_socket.send(rg.encode())
 
-        # Aguarda a resposta do servidor
+        if loginChoice == "1":
+            # Envia User e senha para logar
+            rg = input("Digite o RG da conta: ")
+            senha = input("Digite sua senha:")
+            client_socket.send(senha.encode())
+            client_socket.send(rg.encode())
+
+            loginStatus = client_socket.recv(1024).decode()
+            if(loginStatus == "OK"):
+                response = client_socket.recv(1024).decode()
+                print(f"Resposta do servidor: {response}")
+                # Cliente Logado
+                while True:
+                    # Menu de operações bancárias para o cliente
+                    print("Menu:")
+                    print("1. Consultar saldo")
+                    print("2. Realizar depósito")
+                    print("3. Realizar retirada")
+                    print("4. Realizar transferência")
+                    print("5. Sair")
+                    choice = input("Escolha a operação: ")
+
+                    if choice == "5":
+                        client_socket.send(choice.encode())
+                        break
+
+                    # Envia a escolha do cliente para o servidor
+                    client_socket.send(choice.encode())
+
+                    if choice == "2":
+                        # Solicita o valor a ser depositado
+                        amount = input("Digite o valor: ")
+                        client_socket.send(amount.encode())
+
+                    if choice == "3":
+                        # Solicita o valor a ser retirado
+                        amount = input("Digite o valor: ")
+                        client_socket.send(amount.encode())
+
+                    elif choice == "4":
+                        # Solicita o valor e o número de RG da conta de destino
+                        amount = input("Digite o valor: ")
+                        dest_rg = input("Digite o número de RG da conta de destino: ")
+                        client_socket.send(amount.encode())
+                        client_socket.send(dest_rg.encode())
+
+                    # Aguarda a resposta do servidor após login
+                    response = client_socket.recv(1024).decode()
+                    print(f"Resposta do servidor: {response}")
+
+        # Aguarda a resposta do servidor antes do login
         response = client_socket.recv(1024).decode()
         print(f"Resposta do servidor: {response}")
 
--- server.py
+++ server.py
@@ -15,12 +15,6 @@ def handle_client(client_socket, address):
     global relogio_logico
 
     try:
-        # Simulando autenticação do cliente com RG (número de identidade)
-        rg = client_socket.recv(1024).decode()
-
-        # Inicializando a conta do cliente com saldo zero
-        clientes[rg] = {"rg": rg, "saldo": 1000}
-
         while True:
             data = client_socket.recv(1024).decode()
             if not data:
@@ -28,39 +22,97 @@ def handle_client(client_socket, address):
 
             relogio_logico += 1
 
-            # Processamento das operações bancárias
-            if data == "1":
-                resposta = (
-                    f"Saldo: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
-                )
-            elif data == "2":
-                amount = int(client_socket.recv(1024).decode())
-                if amount <= clientes[rg]["saldo"]:
-                    clientes[rg]["saldo"] -= amount
-                    resposta = f"Retirada de {amount} realizada com sucesso. Novo saldo: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
-                else:
-                    resposta = f"Saldo insuficiente para realizar a retirada. Saldo atual: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
-            elif data == "3":
-                amount = int(client_socket.recv(1024).decode())
-                dest_rg = client_socket.recv(1024).decode()
-                if dest_rg in clientes and amount <= clientes[rg]["saldo"]:
-                    clientes[rg]["saldo"] -= amount
-                    clientes[dest_rg]["saldo"] += amount
-                    resposta = f"Transferência de {amount} realizada para RG {dest_rg} com sucesso. Novo saldo: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
-                elif dest_rg not in clientes:
-                    resposta = f"Conta com RG {dest_rg} não existe. Transferência não realizada. Saldo atual: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
-                else:
-                    resposta = f"Saldo insuficiente para realizar a transferência. Saldo atual: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
-            else:
-                resposta = "Operação inválida. Relógio lógico: {relogio_logico}"
+            # Cadastro
+            if data == "2":
+                # Realizando cadastro do cliente com RG (número de identidade)
+                rg = client_socket.recv(1024).decode()
+                senha = client_socket.recv(1024).decode()
 
-            client_socket.send(resposta.encode())
+                # Verifica se nao existe cadastro com RG
+                if(rg not in clientes):
+                    # Inicializando a conta do cliente com saldo zero
+                    clientes[rg] = {"rg": rg, "senha": senha, "saldo": 0}
+
+                    resposta = f"Conta criada com sucesso!. Relógio lógico: {relogio_logico}"
+                else:
+                    resposta = f"ERRO: Já existe uma conta com esse RG!. Relógio lógico: {relogio_logico}"
+
+                client_socket.send(resposta.encode())
+
+            if data == "1":
+                # Realizando autenticação do cliente com RG (número de identidade)
+                rg = client_socket.recv(1024).decode()
+                senha = client_socket.recv(1024).decode()
+
+                # Verifica se o RG está cadastrado
+                if(rg in clientes):
+                    # Verificando se senha está correta
+                    if(senha == clientes[rg]['senha']):
+                        resposta = "OK"
+                        client_socket.send(resposta.encode())
+                        resposta = f"Login realizado com sucesso. Relógio lógico: {relogio_logico}"
+                        client_socket.send(resposta.encode())
+                        while True:
+                            data = client_socket.recv(1024).decode()
+                            if not data:
+                                break
+
+                            relogio_logico += 1
+
+                            # Processamento das operações bancárias
+                            if data == "1":
+                                resposta = (
+                                    f"Saldo: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
+                                )
+                            elif data == "2":
+                                amount = int(client_socket.recv(1024).decode())
+                                if amount > 0:
+                                    clientes[rg]["saldo"] += amount
+                                    resposta = f"Depósito de {amount} realizado com sucesso. Novo saldo: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
+                                else:
+                                    resposta = f"Valor de depósito inválido. Saldo atual: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
+                            elif data == "3":
+                                amount = int(client_socket.recv(1024).decode())
+                                if amount <= clientes[rg]["saldo"]:
+                                    clientes[rg]["saldo"] -= amount
+                                    resposta = f"Retirada de {amount} realizada com sucesso. Novo saldo: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
+                                else:
+                                    resposta = f"Saldo insuficiente para realizar a retirada. Saldo atual: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
+                            elif data == "4":
+                                amount = int(client_socket.recv(1024).decode())
+                                dest_rg = client_socket.recv(1024).decode()
+                                if dest_rg in clientes and amount <= clientes[rg]["saldo"]:
+                                    clientes[rg]["saldo"] -= amount
+                                    clientes[dest_rg]["saldo"] += amount
+                                    resposta = f"Transferência de {amount} realizada para RG {dest_rg} com sucesso. Novo saldo: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
+                                elif dest_rg not in clientes:
+                                    resposta = f"Conta com RG {dest_rg} não existe. Transferência não realizada. Saldo atual: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
+                                else:
+                                    resposta = f"Saldo insuficiente para realizar a transferência. Saldo atual: {clientes[rg]['saldo']}. Relógio lógico: {relogio_logico}"
+                            elif data == "5":
+                                resposta = f"Fazendo logoff. Relógio lógico: {relogio_logico}"
+                                client_socket.send(resposta.encode())
+                                break
+                            else:
+                                resposta = f"Operação inválida. Relógio lógico: {relogio_logico}"
+
+                            client_socket.send(resposta.encode())
+                    else:
+                        resposta = "ERRO"
+                        client_socket.send(resposta.encode())
+                        resposta = f"Senha incorreta! Relógio lógico: {relogio_logico}"
+                        client_socket.send(resposta.encode())
+                else:
+                    resposta = "ERRO"
+                    client_socket.send(resposta.encode())
+                    resposta = f"Conta não existe. Relógio lógico: {relogio_logico}"
+                    client_socket.send(resposta.encode())
 
     except Exception as e:
         print(f"Erro: {str(e)}")
     finally:
-        if rg in clientes:
-            del clientes[rg]
+        #if rg in clientes:
+            #del clientes[rg]
         print(f"Conexão encerrada com {address}")
         client_socket.close()
 
